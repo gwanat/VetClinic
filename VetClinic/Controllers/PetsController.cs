9342f29976f1fde3de6ff44f42ca0e45f655c489
Gabriela Marek <148369562+GabrielaMarek@users.noreply.github.com>
2024-06-15 07:28:20 +0200

    views

---
+++ VetClinic/Controllers/PetsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VetClinic.Models;
+
+namespace VetClinic.Controllers
+{
+    public class PetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var pet = new Pet { PetId = id.HasValue ? id.Value : 0 };
+
+            return View(pet);
+        }
+
+    }
+}
