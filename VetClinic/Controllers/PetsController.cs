2d5d95e5b1517892c531df75321c47054114a608
Gabriela Marek <148369562+GabrielaMarek@users.noreply.github.com>
2024-06-19 10:51:50 +0200

    update

--- VetClinic/Controllers/PetsController.cs
+++ VetClinic/Controllers/PetsController.cs
@@ -7,15 +7,50 @@ namespace VetClinic.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var pets = PetsRepository.GetPets();
+            return View(pets);
         }
+
 
         public IActionResult Edit(int? id)
         {
-            var pet = new Pet { PetId = id.HasValue ? id.Value : 0 };
+            var pet = PetsRepository.GetPetById(id.HasValue ? id.Value : 0 );
 
             return View(pet);
         }
 
+        [HttpPost]
+        public IActionResult Edit(Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                PetsRepository.UpdatePet(pet.PetId, pet);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pet);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                PetsRepository.AddPet(pet);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(pet);
+        }
+
+        public IActionResult Delete(int petId)
+        {
+            PetsRepository.DeletePet(petId);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- VetClinic/Models/Pet.cs
+++ VetClinic/Models/Pet.cs
@@ -1,9 +1,12 @@
-﻿namespace VetClinic.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VetClinic.Models
 {
     public class Pet
     {
         public int PetId { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
     }
 }
