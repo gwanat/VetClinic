42344a1af22d641dc66558a9202f4b2e91014480
Gabriela Marek <148369562+GabrielaMarek@users.noreply.github.com>
2025-02-04 19:27:19 +0100

    translations

--- VetClinic/Resources/Resource.fr.Designer.cs
+++ VetClinic/Resources/Resource.fr.Designer.cs
@@ -22,14 +22,14 @@ namespace VetClinic.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resource {
+    public class Resource___Copy {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resource() {
+        internal Resource___Copy() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace VetClinic.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VetClinic.Resources.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VetClinic.Resources.Resource - Copy", typeof(Resource___Copy).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
